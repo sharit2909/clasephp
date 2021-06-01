2fbfaf829df33612dbf83fb8889e1e9f545daf1e
sharit2909 <82181444+sharit2909@users.noreply.github.com>
2021-06-01 17:58:33 -0500

    Add files via upload

---
+++ clasephp/metodos/mostrar.php
@@ -0,0 +1,23 @@
+<?php
+
+    if(isset($_POST['name']) && isset($_POST['lastName']) && isset($_POST['email'])){
+        if ($_POST['name'] !== "" && $_POST['lastName'] !=="" && $_POST['email'] !==""){
+            $value = 1;
+        } else {
+            $value = 0;
+        }
+    }
+    echo $lastName."value";
+    header("Location: ./index.php?confirm=".$value)
+?><?php
+
+    if(isset($_POST['name']) && isset($_POST['lastName']) && isset($_POST['email'])){
+        if ($_POST['name'] !== "" && $_POST['lastName'] !=="" && $_POST['email'] !==""){
+            $value = 1;
+        } else {
+            $value = 0;
+        }
+    }
+    echo $lastName."value";
+    header("Location: ./index.php?confirm=".$value)
+?>
